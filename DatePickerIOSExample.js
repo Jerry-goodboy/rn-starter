c6cc5bec388f384b1d760717b5be504b967db9b7
Jerry-goodboy <lvtong521@126.com>
2016-07-27 22:41:43 +0800

    image done

---
+++ DatePickerIOSExample.js
@@ -0,0 +1,137 @@
+import React, { Component } from 'react';
+import {
+  DatePickerIOS,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from 'react-native';
+
+class DatePickerIOSExample extends Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      date: new Date(),
+      timeZoneOffsetInHours: (-1) * (new Date()).getTimezoneOffset() / 60,
+    };
+    this.onDateChange = this.onDateChange.bind(this);
+    this.onTimezoneChange = this.onTimezoneChange.bind(this);
+  }
+
+  onDateChange(date) {
+    this.setState({date: date});
+  }
+
+  onTimezoneChange(event) {
+    var offset = parseInt(event.nativeEvent.text, 10);
+    if (isNaN(offset)) {
+      return;
+    }
+    this.setState({timeZoneOffsetInHours: offset});
+  }
+  render() {
+    // Ideally, the timezone input would be a picker rather than a
+    // text input, but we don't have any pickers yet :(
+    return (
+      <View>
+        <WithLabel label="Value:">
+          <Text>{
+            this.state.date.toLocaleDateString() +
+            ' ' +
+            this.state.date.toLocaleTimeString()
+          }</Text>
+        </WithLabel>
+        <WithLabel label="Timezone:">
+          <TextInput
+            onChange={this.onTimezoneChange}
+            style={styles.textinput}
+            value={this.state.timeZoneOffsetInHours.toString()}
+          />
+          <Text> hours from UTC</Text>
+        </WithLabel>
+        <Heading label="Date + time picker" />
+        <DatePickerIOS
+          date={this.state.date}
+          mode="datetime"
+          timeZoneOffsetInMinutes={this.state.timeZoneOffsetInHours * 60}
+          onDateChange={this.onDateChange}
+        />
+        <Heading label="Date picker" />
+        <DatePickerIOS
+          date={this.state.date}
+          mode="date"
+          timeZoneOffsetInMinutes={this.state.timeZoneOffsetInHours * 60}
+          onDateChange={this.onDateChange}
+        />
+        <Heading label="Time picker, 10-minute interval" />
+        <DatePickerIOS
+          date={this.state.date}
+          mode="time"
+          timeZoneOffsetInMinutes={this.state.timeZoneOffsetInHours * 60}
+          onDateChange={this.onDateChange}
+          minuteInterval={10}
+        />
+      </View>
+    );
+  }
+}
+
+class WithLabel extends Component {
+  render() {
+    return (
+      <View style={styles.labelContainer}>
+        <View style={styles.labelView}>
+          <Text style={styles.label}>
+            {this.props.label}
+          </Text>
+        </View>
+        {this.props.children}
+      </View>
+    );
+  }
+}
+
+class Heading extends Component {
+  render() {
+    return (
+      <View style={styles.headingContainer}>
+        <Text style={styles.heading}>
+          {this.props.label}
+        </Text>
+      </View>
+    );
+  }
+}
+
+var styles = StyleSheet.create({
+  textinput: {
+    height: 26,
+    width: 50,
+    borderWidth: 0.5,
+    borderColor: '#0f0f0f',
+    padding: 4,
+    fontSize: 13,
+  },
+  labelContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginVertical: 2,
+  },
+  labelView: {
+    marginRight: 10,
+    paddingVertical: 2,
+  },
+  label: {
+    fontWeight: '500',
+  },
+  headingContainer: {
+    padding: 4,
+    backgroundColor: '#f6f7f8',
+  },
+  heading: {
+    fontWeight: '500',
+    fontSize: 14,
+  },
+});
+
+export {DatePickerIOSExample};
--- index.ios.js
+++ index.ios.js
@@ -4,24 +4,57 @@
  * @flow
  */
 
+
 import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
   Text,
+  Image,
+  TouchableHighlight,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+  Animated,
+  LayoutAnimation,
+  NavigationExperimental,
+  PixelRatio,
+  ScrollView,
+  ActivityIndicator,
   View
 } from 'react-native';
 
+import {DatePickerIOSExample} from './DatePickerIOSExample.js'
+
 class AwesomeProject extends Component {
+  _onPressButton() {
+    alert("You tapped the button!");
+  }
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
           Welcome to React Native!
         </Text>
+        <TouchableHighlight onPress={this._onPressButton}>
+          <Text>TouchableHighlight</Text>
+        </TouchableHighlight>
+        {/*<TouchableNativeFeedback onPress={this._onPressButton}>
+          <Text>TouchableNativeFeedback only for Android</Text>
+        </TouchableNativeFeedback>*/}
+        <TouchableOpacity onPress={this._onPressButton}>
+          <Text>TouchableOpacity</Text>
+        </TouchableOpacity>
+        {/*<TouchableWithoutFeedback onPress={this._onPressButton}>
+          <Text>TouchableWithoutFeedback</Text>
+        </TouchableWithoutFeedback>*/}
         <Text style={styles.instructions}>
           To get started, edit index.ios.js
         </Text>
+        <Image source={{uri: 'https://facebook.github.io/react/img/logo_og.png'}} style={{width: 200, height: 200}} >
+          <Text>Inside</Text>
+          <Text>Inside</Text>
+        </Image>
+        <Playground></Playground>
         <Text style={styles.instructions}>
           Press Cmd+R to reload,{'\n'}
           Cmd+D or shake for dev menu
@@ -31,10 +64,34 @@ class AwesomeProject extends Component {
   }
 }
 
+const {
+  CardStack: NavigationCardStack,
+  StateUtils: NavigationStateUtils,
+} = NavigationExperimental;
+
 const styles = StyleSheet.create({
+  navigator: {
+    flex: 1,
+  },
+  scrollView: {
+    marginTop: 64
+  },
+  row: {
+    padding: 15,
+    backgroundColor: 'white',
+    borderBottomWidth: 1 / PixelRatio.get(),
+    borderBottomColor: '#CDCDCD',
+  },
+  rowText: {
+    fontSize: 17,
+  },
+  buttonText: {
+    fontSize: 17,
+    fontWeight: '500',
+  },
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'space-around',
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
@@ -47,7 +104,215 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     color: '#333333',
     marginBottom: 5,
-  },
+  }
 });
 
-AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
+class Playground extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bounceValue: new Animated.Value(0),
+    };
+  }
+  render() {
+    return (
+      <Animated.Image                         // Base: Image, Text, View
+        source={{uri: 'https://facebook.github.io/react/img/logo_og.png'}}
+        style={{
+          flex: 1,
+          width: 300,
+          transform: [                        // `transform` is an ordered array
+            {scale: this.state.bounceValue},  // Map `bounceValue` to `scale`
+          ]
+        }}
+      />
+    );
+  }
+  componentDidMount() {
+    this.state.bounceValue.setValue(1.5);     // Start large
+    Animated.spring(                          // Base: spring, decay, timing
+      this.state.bounceValue,                 // Animate `bounceValue`
+      {
+        toValue: 0.8,                         // Animate to smaller size
+        friction: 5,                          // Bouncier spring
+        tension:40
+      }
+    ).start();                                // Start the animation
+    // Animated.sequence([            // spring to start and twirl after decay finishes
+    //   Animated.decay(position, {   // coast to a stop
+    //     velocity: {x: gestureState.vx, y: gestureState.vy}, // velocity from gesture release
+    //     deceleration: 0.997,
+    //   }),
+    //   Animated.parallel([          // after decay, in parallel:
+    //     Animated.spring(position, {
+    //       toValue: {x: 0, y: 0}    // return to start
+    //     }),
+    //     Animated.timing(twirl, {   // and twirl
+    //       toValue: 360,
+    //     }),
+    //   ]),
+    // ]).start();
+  }
+}
+
+// Step 1. Define Initial State and Top Level Component
+class BleedingEdgeApplication extends Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      // This defines the initial navigation state.
+      navigationState: {
+        index: 0, // Starts with first route focused.
+        routes: [{key: 'My Initial Scene'}], // Starts with only one route.
+      },
+    };
+
+    // We'll define this function later - hang on
+    this._onNavigationChange = this._onNavigationChange.bind(this);
+  }
+
+  // Step 2. Reducing the Navigation State
+  _onNavigationChange(type) {
+    // Extract the navigationState from the current state:
+    let {navigationState} = this.state;
+
+    switch (type) {
+      case 'push':
+        // Push a new route, which in our case is an object with a key value.
+        // I am fond of cryptic keys (but seriously, keys should be unique)
+        const route = {key: 'Route-' + Date.now()};
+
+        // Use the push reducer provided by NavigationStateUtils
+        navigationState = NavigationStateUtils.push(navigationState, route);
+        break;
+
+      case 'pop':
+        // Pop the current route using the pop reducer.
+        navigationState = NavigationStateUtils.pop(navigationState);
+        break;
+    }
+
+    // NavigationStateUtils gives you back the same `navigationState` if nothing
+    // has changed. We will only update state if it has changed.
+    if (this.state.navigationState !== navigationState) {
+      // Always use setState() when setting a new state!
+      this.setState({navigationState});
+      // If you are new to ES6, the above is equivalent to:
+      // this.setState({navigationState: navigationState});
+    }
+  }
+
+  render() {
+    return (
+      <MyVerySimpleNavigator
+        navigationState={this.state.navigationState}
+        onNavigationChange={this._onNavigationChange}
+        onExit={this._exit}
+      />
+    );
+  }
+
+}
+
+// Step 3. Define Scenes
+class TappableRow extends Component {
+  render() {
+    return (
+      <TouchableHighlight
+        style={styles.row}
+        underlayColor="#D0D0D0"
+        onPress={this.props.onPress}>
+        <Text style={styles.buttonText}>
+          {this.props.text}
+        </Text>
+      </TouchableHighlight>
+    );
+  }
+}
+
+class MyVeryComplexScene extends Component {
+  render() {
+    return (
+      <ScrollView style={styles.scrollView}>
+        <Text style={styles.row}>
+          Route: {this.props.route.key}
+        </Text>
+        <ActivityIndicator>
+
+        </ActivityIndicator>
+        <DisplayAnImage></DisplayAnImage>
+        <DatePickerIOSExample></DatePickerIOSExample>
+        <TappableRow
+          text="Tap me to load the next scene"
+          onPress={this.props.onPushRoute}
+        />
+        <TappableRow
+          text="Tap me to go back"
+          onPress={this.props.onPopRoute}
+        />
+      </ScrollView>
+    );
+  }
+}
+
+// Step 4. Create a Navigation Stack
+class MyVerySimpleNavigator extends Component {
+
+  // This sets up the methods (e.g. Pop, Push) for navigation.
+  constructor(props, context) {
+    super(props, context);
+
+    this._onPushRoute = this.props.onNavigationChange.bind(null, 'push');
+    this._onPopRoute = this.props.onNavigationChange.bind(null, 'pop');
+
+    this._renderScene = this._renderScene.bind(this);
+  }
+
+  // Now we finally get to use the `NavigationCardStack` to render the scenes.
+  render() {
+    return (
+      <NavigationCardStack
+        onNavigateBack={this._onPopRoute}
+        navigationState={this.props.navigationState}
+        renderScene={this._renderScene}
+        style={styles.navigator}
+      />
+    );
+  }
+
+  // Render a scene for route.
+  // The detailed spec of `sceneProps` is defined at `NavigationTypeDefinition`
+  // as type `NavigationSceneRendererProps`.
+  // Here you could choose to render a different component for each route, but
+  // we'll keep it simple.
+  _renderScene(sceneProps) {
+    return (
+      <MyVeryComplexScene
+        route={sceneProps.scene.route}
+        onPushRoute={this._onPushRoute}
+        onPopRoute={this._onPopRoute}
+        onExit={this.props.onExit}
+      />
+    );
+  }
+}
+
+class DisplayAnImage extends Component {
+  render() {
+    return (
+      <View>
+        <Image
+          source={require('./img/front.png')}
+        />
+        <Image
+          source={{uri: 'https://facebook.github.io/react/img/logo_og.png'}}
+          style={{width: 200, height: 200}}
+        />
+      </View>
+    );
+  }
+}
+
+// AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
+AppRegistry.registerComponent('AwesomeProject', () => BleedingEdgeApplication);
